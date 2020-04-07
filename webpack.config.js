93855584ebd55742e58a0527f5e90fcbb3b1b602
Artem Rakov <scoff1k@gmail.com>
2020-04-07 12:15:11 -0400

    added tempalte for html

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,21 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'template.html',
     }),
   ],
 };
