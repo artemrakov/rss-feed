308133d9dae6b5b8999ce2403de6a77acf310837
Artem Rakov <scoff1k@gmail.com>
2020-04-20 18:02:22 -0400

    setup rss

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { watch } from 'melanke-watchjs';
+// import { watch } from 'melanke-watchjs';
 import axios from 'axios';
 
 const corsUrl = 'https://cors-anywhere.herokuapp.com/';
 
+const extractRssItems = (response) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(response.data, 'application/xml');
+
+  const items = doc.querySelectorAll('item');
+  return [...items].map((item) => {
+    const title = item.querySelector('title').textContent;
+    const link = item.querySelector('link').textContent;
+    const description = item.querySelector('description').textContent;
+
+    return { title, link, description };
+  });
+};
+
 const app = () => {
   const state = {
-    rssLinks: [],
+    rssItems: [],
+    rssUrls: [],
     form: {
-      textField: ''
+      textField: '',
     },
-    request: 'initialized'
+    request: {
+      status: 'initialized',
+      error: '',
+    },
   };
 
   const rssInput = document.getElementById('rssInput');
-  rssInput.addEventListener("input", (event) => {
+  rssInput.addEventListener('input', (event) => {
     state.form.textField = event.target.value;
   });
 
-  const form = document.querySelector(".rss-form");
-  form.addEventListener("submit", (event) => {
+  const form = document.querySelector('.rss-form');
+  form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(state.form.textField);
-    axios({
-      method: 'get',
-      url: corsUrl + state.form.textField,
-      responseType: 'xml'
-    })
+    axios.get(corsUrl + state.form.textField)
       .then((response) => {
-        console.log(response);
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(response.data, 'application/xml');
-        console.log(doc);
+        state.rssItems = extractRssItems(response);
+        state.rssUrls = [...state.rssUrls, state.form.textField];
       })
       .catch((error) => {
         console.error(error);
